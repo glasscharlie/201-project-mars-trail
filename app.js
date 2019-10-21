d96224463213be666e8dbd4759b35ff1df348a1f
egconley <31393715+egconley@users.noreply.github.com>
2019-10-21 14:38:59 -0700

    changes to delete

---
+++ app.js
@@ -0,0 +1,8 @@
+var leftCardEl = document.getElementById('left');
+var centerCardEl = document.getElementById('center');
+var rightCardEl = document.getElementById('right');
+var containerEl = document.getElementById('imageContainer');
+
+
+leftCardEl.name = 'can i do this';
+console.log(leftCardEl.name);
