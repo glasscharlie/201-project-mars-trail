5d0a9de59eae1a17b768f5b83725f4b6d03a04ed
AstroKD <kdreyerinseattle@gmail.com>
2019-10-22 14:08:15 -0700

    card js attached togame html and var add

--- app.js
+++ app.js
@@ -103,44 +103,42 @@ function Card(text, cardRound, gameOver, cardID) {
   this.gameOver = gameOver;
   allCards.push(this);
 }
-
-// Text from http://saganipsum.com/
-new Card('Good Extraplanetary the ash of stellar alchemy rich in mystery Tunguska event star stuff harvesting star light light years? Made in the interiors of collapsing stars stirred by starlight emerged into consciousness permanence of the stars are creatures of the cosmos inconspicuous motes of rock and gas.', 1, false, '1A');
-new Card('Good Kindling the energy hidden in matter colonies hearts of the stars laws of physics take root and flourish made in the interiors of collapsing stars? Something incredible is waiting to be known how far away realm of the galaxies another world something incredible is waiting to be known are creatures of the cosmos?', 1, false, '1B');
-new Card('Good Gathered by gravity across the centuries Apollonius of Perga ship of the imagination extraplanetary brain is the seed of intelligence. A very small stage in a vast cosmic arena Sea of Tranquility the carbon in our apple pies made in the interiors of collapsing stars the sky calls to us another world.', 1, false, '1C');
-new Card('Bad Star stuff harvesting star light cosmos Drake Equation Vangelis are creatures of the cosmos corpus callosum. Realm of the galaxies made in the interiors of collapsing stars tesseract stirred by starlight realm of the galaxies dream of the mind\'s eye? Something incredible is waiting to be known how far away citizens of distant epochs the ash of stellar alchemy rich in heavy atoms not a sunrise but a galaxyrise?', 1, true, '1D');
-new Card('Bad Prime number emerged into consciousness courage of our questions cosmic ocean explorations Rig Veda. Laws of physics stirred by starlight paroxysm of global death the only home we\'ve ever known with pretty stories for which there\'s little good evidence dispassionate extraterrestrial observer.', 1, true, '1E');
-
-// Text from https://loremipsum.io/generator/?n=5&t=p
-new Card('Good Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed risus ultricies tristique nulla aliquet enim. Nisl nunc mi ipsum faucibus vitae aliquet nec.', 2, false, '2A');
-new Card('Good Feugiat scelerisque varius morbi enim nunc. Eget felis eget nunc lobortis mattis aliquam. Ac auctor augue mauris augue neque. Enim ut sem viverra aliquet. Odio morbi quis commodo odio. Netus et malesuada fames ac turpis egestas. Eu scelerisque felis imperdiet proin fermentum leo. Risus quis varius quam quisque id.', 2, false, '2B');
-new Card('Bad Nullam ac tortor vitae purus faucibus ornare. Eget magna fermentum iaculis eu non diam phasellus vestibulum. Ullamcorper a lacus vestibulum sed. Gravida quis blandit turpis cursus in hac habitasse. Scelerisque felis imperdiet proin fermentum. Mauris sit amet massa vitae tortor condimentum lacinia quis vel. Aliquet sagittis id consectetur purus ut faucibus pulvinar elementum.', 2, true, '2C');
-new Card('Bad Parturient montes nascetur ridiculus mus. Cursus metus aliquam eleifend mi in nulla. Cursus vitae congue mauris rhoncus aenean vel elit scelerisque mauris. Etiam sit amet nisl purus in mollis nunc sed id. Non consectetur a erat nam at lectus urna duis. Bibendum est ultricies integer quis auctor elit.', 2, true, '2D');
-new Card('Bad Phasellus egestas tellus rutrum tellus pellentesque. Pellentesque massa placerat duis ultricies lacus sed turpis tincidunt id. Accumsan sit amet nulla facilisi morbi tempus iaculis urna id. Amet commodo nulla facilisi nullam vehicula.', 2, true, '2E');
-
-new Card('Good Mauris in aliquam sem fringilla. Ultrices dui sapien eget mi proin sed libero enim sed. Turpis massa sed elementum tempus egestas. Id semper risus in hendrerit gravida rutrum. Cursus in hac habitasse platea dictumst quisque sagittis. Tristique risus nec feugiat in. Tellus molestie nunc non blandit massa enim nec dui nunc.', 3, false, '3A');
-new Card('Good Faucibus in ornare quam viverra orci sagittis eu. Cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum. Aliquam vestibulum morbi blandit cursus risus at ultrices. Ac turpis egestas maecenas pharetra convallis posuere morbi.', 3, false, '3B');
-new Card('Bad Phasellus egestas tellus rutrum tellus pellentesque. Pellentesque massa placerat duis ultricies lacus sed turpis tincidunt id. Accumsan sit amet nulla facilisi morbi tempus iaculis urna id. Amet commodo nulla facilisi nullam vehicula. ', 3, true, '3C');
-new Card('Bad Volutpat consequat mauris nunc congue nisi vitae suscipit. Nunc sed blandit libero volutpat. Consectetur adipiscing elit duis tristique sollicitudin nibh sit amet. Id velit ut tortor pretium viverra suspendisse potenti nullam ac.', 3, true, '3D');
-new Card('Bad Pellentesque massa placerat duis ultricies lacus sed turpis tincidunt id. Eu augue ut lectus arcu bibendum. Purus gravida quis blandit turpis cursus. Sit amet justo donec enim diam vulputate ut. Tempus imperdiet nulla malesuada pellentesque elit eget gravida cum sociis.', 3, true, '3E');
-
-new Card('Good Mauris in aliquam sem fringilla. Ultrices dui sapien eget mi proin sed libero enim sed. Turpis massa sed elementum tempus egestas. Id semper risus in hendrerit gravida rutrum. Cursus in hac habitasse platea dictumst quisque sagittis. Tristique risus nec feugiat in. Tellus molestie nunc non blandit massa enim nec dui nunc.', 4, false, '4A');
-new Card('Good Faucibus in ornare quam viverra orci sagittis eu. Cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum. Aliquam vestibulum morbi blandit cursus risus at ultrices. Ac turpis egestas maecenas pharetra convallis posuere morbi.', 4, false, '4B');
-new Card('Bad Phasellus egestas tellus rutrum tellus pellentesque. Pellentesque massa placerat duis ultricies lacus sed turpis tincidunt id. Accumsan sit amet nulla facilisi morbi tempus iaculis urna id. Amet commodo nulla facilisi nullam vehicula. ', 4, true, '4C');
-new Card('Bad Volutpat consequat mauris nunc congue nisi vitae suscipit. Nunc sed blandit libero volutpat. Consectetur adipiscing elit duis tristique sollicitudin nibh sit amet. Id velit ut tortor pretium viverra suspendisse potenti nullam ac.', 4, true, '4D');
-new Card('Bad Pellentesque massa placerat duis ultricies lacus sed turpis tincidunt id. Eu augue ut lectus arcu bibendum. Purus gravida quis blandit turpis cursus. Sit amet justo donec enim diam vulputate ut. Tempus imperdiet nulla malesuada pellentesque elit eget gravida cum sociis.', 4, true, '4E');
-
-new Card('Good Mauris in aliquam sem fringilla. Ultrices dui sapien eget mi proin sed libero enim sed. Turpis massa sed elementum tempus egestas. Id semper risus in hendrerit gravida rutrum. Cursus in hac habitasse platea dictumst quisque sagittis. Tristique risus nec feugiat in. Tellus molestie nunc non blandit massa enim nec dui nunc.', 5, false, '5A');
-new Card('Good Faucibus in ornare quam viverra orci sagittis eu. Cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum. Aliquam vestibulum morbi blandit cursus risus at ultrices. Ac turpis egestas maecenas pharetra convallis posuere morbi.', 5, false, '5B');
-new Card('Bad Phasellus egestas tellus rutrum tellus pellentesque. Pellentesque massa placerat duis ultricies lacus sed turpis tincidunt id. Accumsan sit amet nulla facilisi morbi tempus iaculis urna id. Amet commodo nulla facilisi nullam vehicula. ', 5, true, '5C');
-new Card('Bad Volutpat consequat mauris nunc congue nisi vitae suscipit. Nunc sed blandit libero volutpat. Consectetur adipiscing elit duis tristique sollicitudin nibh sit amet. Id velit ut tortor pretium viverra suspendisse potenti nullam ac.', 5, true, '5D');
-new Card('Bad Pellentesque massa placerat duis ultricies lacus sed turpis tincidunt id. Eu augue ut lectus arcu bibendum. Purus gravida quis blandit turpis cursus. Sit amet justo donec enim diam vulputate ut. Tempus imperdiet nulla malesuada pellentesque elit eget gravida cum sociis.', 5, true, '5E');
-
-new Card('Good Mauris in aliquam sem fringilla. Ultrices dui sapien eget mi proin sed libero enim sed. Turpis massa sed elementum tempus egestas. Id semper risus in hendrerit gravida rutrum. Cursus in hac habitasse platea dictumst quisque sagittis. Tristique risus nec feugiat in. Tellus molestie nunc non blandit massa enim nec dui nunc.', 6, false, '6A');
-new Card('Good Faucibus in ornare quam viverra orci sagittis eu. Cras tincidunt lobortis feugiat vivamus at augue eget arcu dictum. Aliquam vestibulum morbi blandit cursus risus at ultrices. Ac turpis egestas maecenas pharetra convallis posuere morbi.', 6, false, '6B');
-new Card('Bad Phasellus egestas tellus rutrum tellus pellentesque. Pellentesque massa placerat duis ultricies lacus sed turpis tincidunt id. Accumsan sit amet nulla facilisi morbi tempus iaculis urna id. Amet commodo nulla facilisi nullam vehicula. ', 6, true, '6C');
-new Card('Bad Volutpat consequat mauris nunc congue nisi vitae suscipit. Nunc sed blandit libero volutpat. Consectetur adipiscing elit duis tristique sollicitudin nibh sit amet. Id velit ut tortor pretium viverra suspendisse potenti nullam ac.', 6, true, '6D');
-new Card('Bad Pellentesque massa placerat duis ultricies lacus sed turpis tincidunt id. Eu augue ut lectus arcu bibendum. Purus gravida quis blandit turpis cursus. Sit amet justo donec enim diam vulputate ut. Tempus imperdiet nulla malesuada pellentesque elit eget gravida cum sociis.', 6, true, '6E');
+//Round 1
+new Card(`Good ${card1AText}`, 1, false, '1A');
+new Card(`Good ${card1BText}`, 1, false, '1B');
+new Card(`Good ${card1CText}`, 1, false, '1C');
+new Card(`Bad ${card1AText}`, 1, true, '1D');
+new Card(`Bad ${card1BText}`, 1, true, '1E');
+//Round 2
+new Card(`Good ${card2AText}`, 2, false, '2A');
+new Card(`Good ${card2BText}`, 2, false, '2B');
+new Card(`Good ${card2CText}`, 2, false, '2C');
+new Card(`Bad ${card2AText}`, 2, true, '2D');
+new Card(`Bad ${card2BText}`, 2, true, '2E');
+//Round 3
+new Card(`Good ${card3AText}`, 3, false, '3A');
+new Card(`Good ${card3BText}`, 3, false, '3B');
+new Card(`Bad ${card3CText}`, 3, true, '3C');
+new Card(`Bad ${card3AText}`, 3, true, '3D');
+new Card(`Bad ${card3BText}`, 3, true, '3E');
+//Round 4
+new Card(`Good ${card4AText}`, 4, false, '4A');
+new Card(`Good ${card4BText}`, 4, false, '4B');
+new Card(`Bad ${card4CText}`, 4, true, '4C');
+new Card(`Bad ${card4AText}`, 4, true, '4D');
+new Card(`Bad ${card4BText}`, 4, true, '4E');
+//Round 5
+new Card(`Good ${card5AText}`, 5, false, '5A');
+new Card(`Good ${card5BText}`, 5, false, '5B');
+new Card(`Bad ${card5CText}`, 5, true, '5C');
+new Card(`Bad ${card5AText}`, 5, true, '5D');
+new Card(`Bad ${card5BText}`, 5, true, '5E');
+//Round 6
+new Card(`Good ${card6AText}`, 6, false, '6A');
+new Card(`Good ${card6BText}`, 6, false, '6B');
+new Card(`Bad ${card6CText}`, 6, true, '6C');
+new Card(`Bad ${card6AText}`, 6, true, '6D');
+new Card(`Bad ${card6BText}`, 6, true, '6E');
 
 //Put allCards into array of arrays
 function getRounds() {
