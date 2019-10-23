faa5ae17c1d005f2428a766a2d289981bdb9397e
Charles Glass <charlie-glass@live.com>
2019-10-23 10:12:56 -0700

    fixed table sorting

--- scores.js
+++ scores.js
@@ -2,21 +2,36 @@
 
 if (localStorage.Player) {
   var players = JSON.parse(localStorage.Player);
-  console.log (players);
 }
+
+function dynamicSort(property) {
+  var sortOrder = 1;
+  if(property[0] === '-') {
+    sortOrder = -1;
+    property = property.substr(1);
+  }
+  return function (a,b) {
+    var result = (a[property] < b[property]) ? -1 : (a[property] > b[property]) ? 1 : 0;
+    return result * sortOrder;
+  };
+}
+players.sort(dynamicSort('-score'));
+// console.log('test',players);
+
+while(players.length > 10){
+  players.pop();
+}
+// console.log('should be 10',players);
 
 function generateTable() {
 
   var body = document.getElementById('highscores');
-
-
   var tbl = document.createElement('table');
   tableHead();
   var tblBody = document.createElement('tbody');
 
   for (var i = 0; i < players.length; i++) {
     var row = document.createElement('tr');
-
     tblBody.appendChild(row);
 
     for (var j = 0; j < 1; j++){
@@ -37,8 +52,6 @@ function generateTable() {
 
   body.appendChild(tbl);
   tbl.appendChild(tblBody);
-  // tbl.setAttribute('border', 2);
-  // newTH.setAttribute('border', 2);
 }
 generateTable();
 
